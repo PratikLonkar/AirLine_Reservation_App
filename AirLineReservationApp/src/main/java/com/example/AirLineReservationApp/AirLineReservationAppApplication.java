6ef42dd33a62a6997177fb3da183323f7a5859c4
Pratik Lonkar <pratiklonkar17@gmail.com>
2023-03-01 16:11:51 +0530

    AirLineReservationApp Created...

---
+++ AirLineReservationApp/src/main/java/com/example/AirLineReservationApp/AirLineReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AirLineReservationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLineReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirLineReservationAppApplication.class, args);
+	}
+
+}
---
+++ AirLineReservationApp/src/test/java/com/example/AirLineReservationApp/AirLineReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AirLineReservationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirLineReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
